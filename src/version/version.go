7ebca766767d95498ecd8c5e11c62e2e00f89ff2
Substitute Me <geroin228339@yandex.ru>
2026-06-16 14:16:06 +0300

    Delete src directory

--- src/version/version.go
+++
@@ -1,7 +0,0 @@
-package version
-
-const (
-	AppVersion    = "2.0.0-Beta.1"
-	GitHubURL     = "github.com/Glebsolopdf/Virga-player"
-	Notifications = "https://raw.githubusercontent.com/Glebsolopdf/virga-notification/refs/heads/main/notifications.json"
-)
